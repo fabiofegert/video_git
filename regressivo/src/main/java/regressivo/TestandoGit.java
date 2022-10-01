4f16ec6ee3a51c6433f68ebd8da3a06da2451649
fabio.fgrt <dogao_original@hotmail.com>
2022-10-01 17:59:00 -0300

    mesagem ta indo direto

--- regressivo/src/main/java/regressivo/TestandoGit.java
+++ regressivo/src/main/java/regressivo/TestandoGit.java
@@ -8,6 +8,7 @@ public class TestandoGit {
 		System.out.println("git hub");
 		System.out.println("essa porra não foi");
 		System.out.println("agora essa porra foi");
+		System.out.println("agora ta indo direto");
 	}
 
 }
